fe9396c26f338f79dd54724a173ec0c8b1d3aee4
attempt118games <attempt118games@gmail.com>
2025-09-02 22:27:21 -0500

    Delete greyed.glsl

--- greyed.glsl
+++
@@ -1,9 +0,0 @@
-vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
-{
-    vec4 texColor = Texel(tex, texture_coords);
-    
-    // Invert all the RGB components
-    texColor.rgb = texColor.rgb * 0.5;  // Inverts the color (photonegative effect)
-
-    return texColor * color;  // Apply color transformation
-}
